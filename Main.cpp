a5b2a7ff9f6b69d9f03db5bc89f2a3778f3dd5c3
Mikhail E <romandviski@mail.ru>
2021-10-14 17:24:35 +0300

    Создал проект

---
+++ Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	cout << "ѕривет мир!" << endl; // commentary
+	int n = 5;
+	int sum = 0;
+	//for (int i = 1; i <= n; ++i)
+	//cout << sum;
+
+
+
+	cout << endl << endl << endl;
+	system("pause"); // что бы не закрывалась консоль
+	return 0;
+}
